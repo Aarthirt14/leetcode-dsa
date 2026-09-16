2ef772489d89208f31d92b3ebce47a284e72ef73
Aarthi S <aarthiii333@gmail.com>
2026-09-16 09:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.13%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
@@ -1,12 +1,18 @@
 class Solution:
     def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        answer1=0
-        answer2=0
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        answer1 = 0
+        answer2 = 0
+
         for num in nums1:
-            if num in nums2:
-                answer1+=1
+            if num in set2:
+                answer1 += 1
+
         for num in nums2:
-            if num in nums1:
-                answer2+=1
-        return answer1,answer2
+            if num in set1:
+                answer2 += 1
+
+        return [answer1, answer2]
         
